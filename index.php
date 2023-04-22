945cb7888863b5892a58e87ed022159ce306049e
unixuser <unixuser@example.com>
2023-04-22 19:05:18 +0900

    renamed and modified

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-   //encodes the xml, replaces all invalid characters with their hex value, decodes the xml, then parses it.
-   var encoded = encodeURIComponent(xmlhttp.responseText);
-   var replaced = encoded.replace(/%26/g,"&amp;");  // &
-   var decoded = decodeURIComponent(replaced);
